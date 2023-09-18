4368c56cdb7d3a18d35600ee6f718596a438d29c
Yasser Corzo <yassercorzo1@gmail.com>
2023-09-18 00:54:24 -0400

    first commit

---
+++ hw1/code/visual_words.py
@@ -0,0 +1,186 @@
+import os, multiprocessing
+from os.path import join, isfile
+
+import numpy as np
+from PIL import Image
+import scipy.ndimage
+import skimage.color
+import sklearn.cluster
+from tempfile import TemporaryFile
+
+def extract_filter_responses(opts, img):
+    '''
+    Extracts the filter responses for the given image.
+
+    [input]
+    * opts    : options
+    * img    : numpy.ndarray of shape (H,W) or (H,W,3)
+    [output]
+    * filter_responses: numpy.ndarray of shape (H,W,3F)
+    '''
+    
+    filter_scales = opts.filter_scales
+    # ----- TODO -----
+    
+    # if image is in greyscale, duplicate them into 3 channels
+    if img.ndim != 3:
+        grey_img = np.zeros_like(img)
+        grey_img[:, :, 0] = img
+        grey_img[:, :, 1] = img
+        grey_img[:, :, 2] = img
+        img = grey_img
+    
+    # convert image into Lab color space
+    img = skimage.color.rgb2lab(img)
+    
+    # retrieve LAB channels
+    L = img[:, :, 0]
+    A = img[:, :, 1]
+    B = img[:, :, 2]
+    
+    filter_responses = []
+    
+    # convolve all filters with the image with all scales. Scales are 1 and 2. Filters are:
+    # (1) Gaussian, (2) Laplacian of Gaussian, (3) derivative of Gaussian in the x direction, and (4) derivative of Gaussian in the y direction
+    for scale in filter_scales:
+        # apply filter (1) on every channel
+        l_filter_response = scipy.ndimage.gaussian_filter(L, sigma=scale, mode='nearest')
+        a_filter_response = scipy.ndimage.gaussian_filter(A, sigma=scale, mode='nearest')
+        b_filter_response = scipy.ndimage.gaussian_filter(B, sigma=scale, mode='nearest')
+        filter_responses.append(l_filter_response)
+        filter_responses.append(a_filter_response)
+        filter_responses.append(b_filter_response)
+        # apply filter (2)
+        l_filter_response = scipy.ndimage.gaussian_laplace(L, sigma=scale, mode='nearest')
+        a_filter_response = scipy.ndimage.gaussian_laplace(A, sigma=scale, mode='nearest')
+        b_filter_response = scipy.ndimage.gaussian_laplace(B, sigma=scale, mode='nearest')
+        filter_responses.append(l_filter_response)
+        filter_responses.append(a_filter_response)
+        filter_responses.append(b_filter_response)
+        # apply filter (3)
+        l_filter_response = scipy.ndimage.gaussian_filter(L, sigma=scale, order=1, axes=1, mode='nearest')
+        a_filter_response = scipy.ndimage.gaussian_filter(A, sigma=scale, order=1, axes=1, mode='nearest')
+        b_filter_response = scipy.ndimage.gaussian_filter(B, sigma=scale, order=1, axes=1, mode='nearest')
+        filter_responses.append(l_filter_response)
+        filter_responses.append(a_filter_response)
+        filter_responses.append(b_filter_response)
+        # apply filter (4)
+        l_filter_response = scipy.ndimage.gaussian_filter(L, sigma=scale, order=1, axes=0, mode='nearest')
+        a_filter_response = scipy.ndimage.gaussian_filter(A, sigma=scale, order=1, axes=0, mode='nearest')
+        b_filter_response = scipy.ndimage.gaussian_filter(B, sigma=scale, order=1, axes=0, mode='nearest')
+        filter_responses.append(l_filter_response)
+        filter_responses.append(a_filter_response)
+        filter_responses.append(b_filter_response)
+        
+    filter_responses = np.stack(filter_responses, axis=2)
+    return filter_responses
+
+def compute_dictionary_one_image(args):
+    '''
+    Extracts a random subset of filter responses of an image and save it to disk
+    This is a worker function called by compute_dictionary
+
+    Your are free to make your own interface based on how you implement compute_dictionary
+    '''
+
+    # ----- TODO -----
+    file_num, train_file, opts = args
+    data_dir = opts.data_dir
+    alpha = opts.alpha
+    
+    # read the image
+    img_path = join(data_dir, train_file)   
+    print(img_path, " ", file_num)
+    img = Image.open(img_path)
+    img = np.array(img).astype(np.float32)/255
+    
+    # extract filter response from image
+    filter_response = extract_filter_responses(opts, img)
+    
+    # extract alpha points from filter response
+    x = np.random.randint(0, img.shape[1], size=alpha)
+    y = np.random.randint(0, img.shape[0], size=alpha)
+    points = np.vstack((x, y)).T
+    alpha_points = filter_response[points[:, 1], points[:, 0], :]
+    return [(file_num, alpha_points)]
+    
+
+def compute_dictionary(opts, n_worker=1):
+    '''
+    Creates the dictionary of visual words by clustering using k-means.
+
+    [input]
+    * opts         : options
+    * n_worker     : number of workers to process in parallel
+    
+    [saved]
+    * dictionary : numpy.ndarray of shape (K,3F)
+    '''
+
+    data_dir = opts.data_dir
+    feat_dir = opts.feat_dir
+    out_dir = opts.out_dir
+    K = opts.K
+    alpha = opts.alpha
+    train_files = open(join(data_dir, 'train_files.txt')).read().splitlines()
+    
+    # ----- TODO -----
+    
+    # initalize aggregated filter responses from every image (alpha*T x 3F)
+    filter_bank_num = len(opts.filter_scales) * 4
+    filter_responses = np.zeros((alpha * len(train_files), 3 * filter_bank_num))
+    
+    # Create pool of workers
+    pool = multiprocessing.Pool(n_worker)
+    
+    image_alpha_points = dict()
+    
+    args = [(i, train_file, opts) for (i, train_file) in enumerate(train_files)]
+    
+    overall_dict = pool.map(compute_dictionary_one_image, args)
+
+    for sub_list in overall_dict:
+        tup = sub_list[0]
+        key = tup[0]
+        alpha_points = tup[1]
+        image_alpha_points[key] = alpha_points
+    
+    # loop through every image's filter response with alpha points and collect them
+    for (file_num, path) in enumerate(train_files):
+        alpha_points = image_alpha_points[file_num]
+        filter_responses[file_num * alpha: (file_num + 1) * alpha, :] = alpha_points
+    
+    kmeans = sklearn.cluster.KMeans(n_clusters=K).fit(filter_responses)
+    dictionary = kmeans.cluster_centers_
+    np.save(join(out_dir, 'dictionary.npy'), dictionary)
+    pass
+
+    ## example code snippet to save the dictionary
+    # np.save(join(out_dir, 'dictionary.npy'), dictionary)
+
+def get_visual_words(opts, img, dictionary):
+    '''
+    Compute visual words mapping for the given img using the dictionary of visual words.
+
+    [input]
+    * opts    : options
+    * img    : numpy.ndarray of shape (H,W) or (H,W,3)
+    
+    [output]
+    * wordmap: numpy.ndarray of shape (H,W)
+    '''
+    
+    # ----- TODO -----
+    wordmap = np.zeros((img.shape[0], img.shape[1]))
+    filter_responses = extract_filter_responses(opts, img)
+    rows, cols = filter_responses.shape[0], filter_responses.shape[1]
+    
+    for i in range(rows):
+        for j in range(cols):
+            filter_responses_vec = np.array([filter_responses[i, j, :]])
+            dist_to_visual_words = scipy.spatial.distance.cdist(filter_responses_vec, dictionary, metric='euclidean')
+            closest_visual_word = np.argmin(dist_to_visual_words)
+            wordmap[i, j] = closest_visual_word
+
+    return wordmap
+
