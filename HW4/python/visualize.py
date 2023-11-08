df326d4f06573951c98d3cd8dbec89c0d404352c
Yasser Corzo <yassercorzo1@gmail.com>
2023-11-07 21:39:30 -0500

    began HW4

---
+++ HW4/python/visualize.py
@@ -0,0 +1,20 @@
+'''
+Q4.2:
+    1. Integrating everything together.
+    2. Loads necessary files from ../data/ and visualizes 3D reconstruction using scatter
+'''
+import cv2
+
+from helper import *
+from submission import *
+
+# testing Q2.1
+img1 = cv2.imread('../data/im1.png')
+img2 = cv2.imread('../data/im2.png')
+data = np.load('../data/some_corresp.npz')
+pts1 = data['pts1']
+pts2 = data['pts2']
+
+M = max(img1.shape[0], img1.shape[1])
+F = eightpoint(pts1, pts2, M)
+displayEpipolarF(img1, img2, F)
