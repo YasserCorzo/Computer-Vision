7c62e44918c95fe0a5284f5a1d00aa5046234026
Yasser Corzo <yassercorzo1@gmail.com>
2023-11-27 10:13:25 -0500

    started Q4.4

--- HW5/python/q4.py
+++ HW5/python/q4.py
@@ -20,5 +20,41 @@ def findLetters(image):
     ##########################
     ##### your code here #####
     ##########################
+    
+    # estimate noise
+    sigma_est = skimage.restoration.estimate_sigma(image, channel_axis=-1, average_sigmas=True)
+    
+    # denoise image with estimated sigma
+    gaussian_img = skimage.filters.gaussian(image, sigma=sigma_est)
+    
+    # greyscale image
+    greyscale_img = skimage.color.rgb2gray(gaussian_img)
+    
+    # set threshold to create binary image
+    thresh = skimage.filters.threshold_otsu(greyscale_img)
+    bw = greyscale_img < thresh
+    
+    # morphology (closing)
+    bw = skimage.morphology.closing(bw, skimage.morphology.square(3))
+    
+    # label connected groups of character pixels
+    label_img = skimage.morphology.label(bw, connectivity=2)
+    
+    # find locations of the labels
+    label_props = skimage.measure.regionprops(label_img)
+    
+    # find the mean area of the labeled regions
+    areas = []
+    for region in label_props:
+        areas.append(region.area)
+    mean_area = sum(areas) / len(areas)
+    
+    # set new threshold for mean (after testing mean needs to be smaller)
+    thresh_mean = mean_area / 3
+    
+    # calculate upper left and lower left coords of regions, skip small regions
+    for region in label_props:
+        if region.area >= thresh_mean:
+            bboxes.append(region.bbox)
 
     return bboxes, bw
